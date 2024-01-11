1537a99b96a55d47004c3e8a65df0c75c9304501
Gabriel de Oliveira Lopes <lopesgabriel0199@gmail.com>
2024-01-10 22:16:32 -0300

    added fetch question answers use case

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
@@ -10,6 +11,17 @@ export class InMemoryAnswersRepository implements AnswersRepository {
 
   async findById(answerId: string): Promise<Answer | null> {
     return this.items.find((item) => item.id.toValue() === answerId) ?? null
+  }
+
+  async findManyByQuestionId(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<Answer[]> {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answers
   }
 
   async save(answer: Answer): Promise<void> {
