00ab666e82cc2020a74ba7cfd0029c08c936a2b3
Gabriel de Oliveira Lopes <lopesgabriel0199@gmail.com>
2024-01-29 18:11:31 -0300

    feat: added fetch question comments use case

---
+++ src/domain/forum/application/use-cases/fetch-question-comments.ts
@@ -0,0 +1,31 @@
+import { QuestionComment } from '../../enterprise/entities/question-comment'
+import { QuestionCommentsRepository } from '../repositories/question-comments-repository'
+
+interface FetchQuestionAnswersUseCaseRequest {
+  questionId: string
+  page: number
+}
+
+interface FetchQuestionAnswersUseCaseResponse {
+  questionComments: QuestionComment[]
+}
+
+export class FetchQuestionCommentsUseCase {
+  constructor(
+    private readonly questionCommentsRepository: QuestionCommentsRepository,
+  ) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionAnswersUseCaseRequest): Promise<FetchQuestionAnswersUseCaseResponse> {
+    const questionComments =
+      await this.questionCommentsRepository.findManyByQuestionId(questionId, {
+        page,
+      })
+
+    return {
+      questionComments,
+    }
+  }
+}
